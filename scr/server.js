ca399032cbbbfd9203eeaa93be9e1a904d6de151
JuanCho1977 <gonzalezinsfranjm@gmail.com>
2024-08-01 16:30:20 -0300

    Modificacion
    Agregue una devlucion del ID al cliente
    Modifique el Scrip.js

--- scr/server.js
+++ scr/server.js
@@ -1,6 +1,6 @@
 //Server.js
 
-const PORT = 8080
+const PORT = 8080;
 const express = require('express');
 const app = express();
 const ProductRouter = require('./router/ProductRouter.js');
@@ -12,10 +12,13 @@ const clientManagerFs = require('./managers/FileSystem/clientManagerFs.js');
 
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/static', express.static(__dirname + "Public"));
+
 app.use("api/cliente", ClientRouter);
 app.use("api/product", ProductRouter);
 app.use("api/cart",CartRouter);
-app.use(express.static( "Public"));
+
 
 
 
