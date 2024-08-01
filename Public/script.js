f98f27f64f61b85460674985fe917f7cad59274d
JuanCho1977 <gonzalezinsfranjm@gmail.com>
2024-08-01 17:02:36 -0300

    Modifico Scrip
    corrijo scrip vuelo Console.log que no tiena da que hacer

--- Public/script.js
+++ Public/script.js
@@ -1,36 +1,41 @@
+
+
 const formClient = document.getElementById('form-client');
 const formProduct = document.getElementById('form-product');
 
 
 
 formClient.addEventListener('submit', async (e) => {
+	
 	e.preventDefault();
 	const nombre = document.getElementById('nombre').value;
 	const email = document.getElementById('email').value;
 	const clientId = generateId(); // Generar un ID único
-	function generateId() {
-        return Math.floor(Math.random() * 1000000).toString();
-    }
-    try {
+	document.getElementById('clientId').innerText = clientId
+
+     try {
 		await productManagerFs.createClient({ id: clientId, nombre, email });
 		console.log('Cliente creado con éxito');
-        console.log("Su Id para operar es :", clientId)
-	} catch (error) {
+      
+	 } catch (error) {
 		console.error('Error al crear cliente:', error);
-	}
+	 }
 });
 
 
+function generateId() {
+	return Math.floor(Math.random() * 1000).toString();
 
-
+}
+	
 
 
 formProduct.addEventListener('submit', async (e) => {
 	e.preventDefault();
 	const nombre = document.getElementById('nombre').value;
-	const price = document.getElementById('price').value;
+	const precio = document.getElementById('precio').value;
 	try {
-		await productManagerFs.createProduct({ nombre, price });
+		await productManagerFs.createProduct({ nombre, precio });
 		console.log('Producto creado con éxito');
 	} catch (error) {
 		console.error('Error al crear producto:', error);
